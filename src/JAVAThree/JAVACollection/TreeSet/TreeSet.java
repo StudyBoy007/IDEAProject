aab1bc61d1a00ab0d17f3acb40e893b9fb7a166f
StudyBoy007 <1045112166@qq.com>
2019-06-24 14:07:53 +0800

    changeProject

--- src/JAVAThree/JAVACollection/TreeSet/TreeSet.java
+++ src/JAVAThree/JAVACollection/TreeSet/TreeSet.java
@@ -1,6 +1,8 @@
 package JAVAThree.JAVACollection.TreeSet;
 
 
+import java.util.Random;
+
 /**
  * Create by czq
  * time on 2019/6/21  15:12
@@ -9,12 +11,11 @@ public class TreeSet {
     public static void main(String[] args) {
         java.util.TreeSet treeSet = new java.util.TreeSet();
         Student student1 = new Student(1, "蔡泽球");
-        Student student2 = new Student(2, "李飞");
-        Student student3 = new Student(3, "刘梦昊");
+        Student student2 = new Student(3, "李飞");
+        Student student3 = new Student(2, "刘梦昊");
         treeSet.add(student1);
         treeSet.add(student3);
         treeSet.add(student2);
-//        System.out.println(treeSet);
-
+        System.out.println(treeSet);
     }
 }
