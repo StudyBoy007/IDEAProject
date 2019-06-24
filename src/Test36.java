737511814d274776f9171c6dcfb2bc791e86f3e9
StudyBoy007 <1045112166@qq.com>
2019-06-24 15:41:48 +0800

    version=1.0

---
+++ src/Test36.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * Create by czq
+ * time on 2019/6/24  15:40
+ */
+public class Test36 {
+    public static void main(String[] args) {
+        System.out.println("test36");
+    }
+}
